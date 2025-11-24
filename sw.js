88a1b112b45e1b66896167d787c7c55b8d5b1991
ineko0402 <a.catnapkoala@gmail.com>
2025-11-24 17:11:27 +0900

    add Favicon

---
+++ sw.js
@@ -0,0 +1,32 @@
+// sw.js
+const CACHE_NAME = 'CalcApp-cache-v20251124';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
