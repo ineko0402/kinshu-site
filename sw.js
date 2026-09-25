d3e0f53dd5a82c8dc31d8b6e322b21a97d3ca91c
ineko0402 <52097123+ineko0402@users.noreply.github.com>
2026-09-25 20:44:23 +0900

    ui: animate pinning and manual note reordering (#28)

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 // sw.js
-const CACHE_NAME = 'CalcApp-cache-v20260925.14';
+const CACHE_NAME = 'CalcApp-cache-v20260925.15';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
-  './style.css?v=20260925.14',
-  './css/variables.css?v=20260925.14',
-  './css/base.css?v=20260925.14',
-  './css/effects.css?v=20260925.14',
-  './css/layout.css?v=20260925.14',
-  './css/components.css?v=20260925.14',
-  './css/navigation.css?v=20260925.14',
-  './css/lists.css?v=20260925.14',
-  './css/typography.css?v=20260925.14',
-  './css/keypad.css?v=20260925.14',
-  './css/modals.css?v=20260925.14',
-  './css/withdrawal-fee.css?v=20260925.14',
-  './css/pc.css?v=20260925.14',
+  './style.css?v=20260925.15',
+  './css/variables.css?v=20260925.15',
+  './css/base.css?v=20260925.15',
+  './css/effects.css?v=20260925.15',
+  './css/layout.css?v=20260925.15',
+  './css/components.css?v=20260925.15',
+  './css/navigation.css?v=20260925.15',
+  './css/lists.css?v=20260925.15',
+  './css/typography.css?v=20260925.15',
+  './css/keypad.css?v=20260925.15',
+  './css/modals.css?v=20260925.15',
+  './css/withdrawal-fee.css?v=20260925.15',
+  './css/pc.css?v=20260925.15',
   './manifest.json'
 ];
 
