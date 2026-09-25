82207feddc032b2850998bf165665fb5feb9888a
ineko0402 <52097123+ineko0402@users.noreply.github.com>
2026-09-25 17:48:18 +0900

    fix: prevent sticky bottom navigation outlines on touch (#22)

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 // sw.js
-const CACHE_NAME = 'CalcApp-cache-v20260925.7';
+const CACHE_NAME = 'CalcApp-cache-v20260925.8';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
-  './style.css?v=20260925.7',
-  './css/variables.css?v=20260925.7',
-  './css/base.css?v=20260925.7',
-  './css/effects.css?v=20260925.7',
-  './css/layout.css?v=20260925.7',
-  './css/components.css?v=20260925.7',
-  './css/navigation.css?v=20260925.7',
-  './css/lists.css?v=20260925.7',
-  './css/typography.css?v=20260925.7',
-  './css/keypad.css?v=20260925.7',
-  './css/modals.css?v=20260925.7',
-  './css/withdrawal-fee.css?v=20260925.7',
-  './css/pc.css?v=20260925.7',
+  './style.css?v=20260925.8',
+  './css/variables.css?v=20260925.8',
+  './css/base.css?v=20260925.8',
+  './css/effects.css?v=20260925.8',
+  './css/layout.css?v=20260925.8',
+  './css/components.css?v=20260925.8',
+  './css/navigation.css?v=20260925.8',
+  './css/lists.css?v=20260925.8',
+  './css/typography.css?v=20260925.8',
+  './css/keypad.css?v=20260925.8',
+  './css/modals.css?v=20260925.8',
+  './css/withdrawal-fee.css?v=20260925.8',
+  './css/pc.css?v=20260925.8',
   './manifest.json'
 ];
 
